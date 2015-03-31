d98284b133fb31dac48f1aa4692fe72fb44717b2
Tara Mahoney <tara.mahoney@me.com>
2015-03-31 16:15:38 -0400

    Exercise 2 completed - name is different because I had trouble saving

---
+++ exercise211.rb
@@ -0,0 +1,18 @@
+#1. tip calculator
+
+puts 55 * 0.13
+
+#2.try adding an integer & string, and then convert integer to string.
+
+puts "HERE I AM" + 355.to_s	 
+
+#3. outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+my_number1 = 45628 
+my_number2 = 7839
+
+puts "This is the answer: #{my_number1 * my_number2}"
+
+#4. the value of the following expression will be false
+
+puts (true && false || false && true || !false && false)
